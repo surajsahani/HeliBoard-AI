3832092bfa8f639c2668f5fa5dede62ebabd7fd9
kalyani <Kalyani22@navgurukul.org>
2024-08-13 13:42:18 +0530

    QA fixes

--- app/src/main/java/helium314/keyboard/latin/suggestions/SuggestionStripView.java
+++ app/src/main/java/helium314/keyboard/latin/suggestions/SuggestionStripView.java
@@ -28,7 +28,6 @@ import android.speech.RecognitionListener;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
-import androidx.annotation.RequiresApi;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.text.TextUtils;
@@ -944,7 +943,6 @@ public final class SuggestionStripView extends RelativeLayout implements OnClick
         mMoreSuggestionsView.onHoverEvent(me);
         return true;
     }
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onClick(final View view) {
         AudioAndHapticFeedbackManager.getInstance().performHapticAndAudioFeedback(KeyCode.NOT_SPECIFIED, this);
@@ -1148,7 +1146,6 @@ public final class SuggestionStripView extends RelativeLayout implements OnClick
         colors.setColor(view, ColorType.TOOL_BAR_KEY);
         colors.setBackground(view, ColorType.STRIP_BACKGROUND);
     }
-    @RequiresApi(api = Build.VERSION_CODES.M)
     private boolean isInternetAvailable() {
         Context context = getContext();
         if (context == null) {
@@ -1157,7 +1154,10 @@ public final class SuggestionStripView extends RelativeLayout implements OnClick
         ConnectivityManager connectivityManager =
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null) {
-            Network network = connectivityManager.getActiveNetwork();
+            Network network = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                network = connectivityManager.getActiveNetwork();
+            }
             NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
             return networkCapabilities != null &&
                     networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
