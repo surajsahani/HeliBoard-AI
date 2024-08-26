c03ef62e5a107119b957cfd5b3ac5d0fa71cb926
Dhanshri <dhanshri20@navgurukul.org>
2024-08-26 10:51:22 +0530

    Issue for first word repeating

--- app/src/main/java/org/samyarth/oskey/latin/suggestions/SuggestionStripView.java
+++ app/src/main/java/org/samyarth/oskey/latin/suggestions/SuggestionStripView.java
@@ -282,8 +282,7 @@ public final class SuggestionStripView extends RelativeLayout implements OnClick
             ClipData clip = ClipData.newPlainText("aiOutput", aiOutput.getText().toString());
             clipboard.setPrimaryClip(clip);
             mListener.onCodeInput(KeyCode.CLIPBOARD_PASTE, Constants.SUGGESTION_STRIP_COORDINATE, Constants.SUGGESTION_STRIP_COORDINATE, false);
-            AIOutputEvent event = new AIOutputEvent(aiOutput.getText().toString());
-            EventBus.getDefault().post(event);
+
 
         });
 
@@ -296,11 +295,14 @@ public final class SuggestionStripView extends RelativeLayout implements OnClick
                 ClipData clip = ClipData.newPlainText("aiOutput", updatedText);
                 clipboard.setPrimaryClip(clip);
                 mListener.onCodeInput(KeyCode.CLIPBOARD_PASTE, Constants.SUGGESTION_STRIP_COORDINATE, Constants.SUGGESTION_STRIP_COORDINATE, false);
-
                 AIOutputEvent event = new AIOutputEvent(updatedText);
                 EventBus.getDefault().post(event);
             }
         });
+
+        AIOutputEvent event = new AIOutputEvent(aiOutput.getText().toString());
+        EventBus.getDefault().post(event);
+
     }
 
 //    public void setOutputText(String outputText) {
